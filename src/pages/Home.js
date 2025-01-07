5648a72314217a75c49ee119e44948cac068ee41
Alexandra Kononenko <alexandra.kononenko@salle.url.edu>
2025-01-07 15:04:49 +0100

    add changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,14 +2,55 @@ import React, { useState, useEffect } from "react";
 import { collection, getDocs, addDoc, updateDoc, deleteDoc, doc, setDoc } from "firebase/firestore";
 import { db } from "../services/firebase-config";
 import UserCard from "../components/UserCard";
+
 import politico1 from "../Images/Politicos/politico1.jpg";
 import politico2 from "../Images/Politicos/politico2.jpg";
-// (Resto de las imágenes)
+import politico3 from "../Images/Politicos/politico3.jpg";
+import politico4 from "../Images/Politicos/politico4.jpg";
+import politico5 from "../Images/Politicos/politico5.jpg";
+import politico6 from "../Images/Politicos/politico6.png";
+import politico7 from "../Images/Politicos/politico7.png";
+import politico8 from "../Images/Politicos/politico8.png";
+import politico9 from "../Images/Politicos/politico9.jpg";
+import politico10 from "../Images/Politicos/politico10.webp";
+import politico11 from "../Images/Politicos/politico11.jpg";
+import politico12 from "../Images/Politicos/politico12.jpg";
+import politico13 from "../Images/Politicos/politico13.webp";
+import politico14 from "../Images/Politicos/politico14.jpg";
+import politico15 from "../Images/Politicos/politico15.jpg";
+import politico16 from "../Images/Politicos/politico16.jpg";
+import politico17 from "../Images/Politicos/politico17.jpg";
+import politico18 from "../Images/Politicos/politico18.avif";
+import politico20 from "../Images/Politicos/politico20.jpg";
+import politico21 from "../Images/Politicos/politico21.jpg";
+import politico23 from "../Images/Politicos/politico23.jpg";
+import politico24 from "../Images/Politicos/politico24.jpg";
+import politico25 from "../Images/Politicos/politico25.jpg";
 
 const imagesData = [
   politico1,
   politico2,
-  // (Resto de las imágenes)
+  politico3,
+  politico4,
+  politico5,
+  politico6,
+  politico7,
+  politico8,
+  politico9,
+  politico10,
+  politico11,
+  politico12,
+  politico13,
+  politico14,
+  politico15,
+  politico16,
+  politico17,
+  politico18,
+  politico20,
+  politico21,
+  politico23,
+  politico24,
+  politico25,
 ];
 
 function Home() {
